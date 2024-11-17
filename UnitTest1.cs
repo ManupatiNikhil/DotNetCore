cd71e365cf0c79eed8ca5b571f4a3b7387b37970
Nithish <nikhilmanupati1998@gmail.com~>
2024-11-17 10:28:00 +0530

    SampleDotNetSelenium

---
+++ UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace DotnetSelenium
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://onlinecourses.nptel.ac.in/");
+            Assert.Pass();
+        }
+    }
+}
